a31640c58e685e1fa2fd6a51fc2e7212ab015439
imran786sd <37811409+imran786sd@users.noreply.github.com>
2025-04-14 15:47:10 +0530

    Delete stripe.js

--- stripe.js
+++
@@ -1,8 +0,0 @@
-
-function startStripeCheckout() {
-  alert("Simulated Stripe Checkout (Test Mode)");
-  firebase.firestore().collection("users").doc(currentUser.uid).set({ isPro: true }, { merge: true }).then(() => {
-    alert("Pro Mode Activated!");
-    document.getElementById("download-csv").disabled = false;
-  });
-}
